f389932002b8184f16f96ad833734a66235d2ac9
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-03-27 16:19:14 +0530

    refactoring from functional to class component

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//functional component
+
+// const App = ()=>{
+//      window.navigator.geolocation.getCurrentPosition(
+//         (position)=>console.log(position),
+//         (err)=>console.log(err)
+//     );
+//     return (
+//         <div>Hi there!</div>
+//     );
+
+// }
+
+//class component
+
+class App extends React.Component{
+    render(){
+        window.navigator.geolocation.getCurrentPosition(
+        (position)=>console.log(position),
+        (err)=>console.log(err)
+    );
+
+        return <div>Lattude: </div>
+    }
+}
+
+ReactDOM.render(<App/>,document.querySelector('#root'));
