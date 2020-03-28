7a4141c45c03c8868e1e063078c185694695f904
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-03-28 15:48:24 +0530

    conditionally rendering content

--- src/index.js
+++ src/index.js
@@ -44,13 +44,23 @@ class App extends React.Component{
     //     (err)=>console.log(err)
     // );
 
-        return (
-            <div>
-                Latitude : {this.state.lat}
-                <br/>
-                Error : {this.state.errorMessage}
-            </div>
-        );
+    //    return(
+    //         // <div>
+    //         //     Latitude : {this.state.lat}
+    //         //     <br/>
+    //         //     Error : {this.state.errorMessage}
+    //         // </div>
+    //    );
+
+            if(this.state.lat && !this.state.errorMessage){
+                return <div>Latitude :{this.state.lat}</div>;
+            }
+            if(!this.state.lat && this.state.errorMessage){
+                return <div>Error :{this.state.errorMessage}</div>;
+            }
+
+           return  <div>Loading!</div>;
+       
         
     }
 }
